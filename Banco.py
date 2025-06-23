01c6b6c6f696e611ef49ff70ec5f11eac42c2cde
Fabio Landin <flandin1990@gmail.com>
2025-06-23 18:04:20 -0300

    Refatorando colocando funções

--- Banco.py
+++ Banco.py
@@ -3,13 +3,27 @@ menu = """
 [s] Sacar
 [e] Extrato
 [q] Sair
+\n
 """
 valores = 0
 saldo = 0
 limite_saque = 500
-numero_de_saques = 0
+numero_de_saques = 1
 limite_de_saques = 3
 extrato = []
+
+def depositar(saldo, valores):
+    saldo = saldo + valores
+    extrato.append(f'Depósito: R${valores:.2f}')
+    print(f"Saldo {saldo}")
+    return saldo
+
+def sacar(saldo, valores, numero_de_saques):
+    saldo = saldo - valores
+    numero_de_saques = numero_de_saques + 1
+    extrato.append(f'Saque: R${valores:.2f}')
+    print(f"Saldo {saldo}")
+    return saldo, numero_de_saques
 
 
 while True:
@@ -19,27 +33,20 @@ while True:
     if opcao == 'd':
         print("Deposito \n")
         valores = int(input('Quanto deseja depositar?'))
-        if valores > 0:
-            saldo = saldo + valores
-            extrato.append(f'Depósito: R${valores:.2f}')
-            print(f"Saldo {saldo}")
+        saldo = depositar(saldo, valores)
 
     elif opcao == 's':
         print("Saque \n")
         valores = int(input('Quanto deseja sacar?'))
-
         if valores > limite_saque or numero_de_saques > limite_de_saques or valores > saldo:
             print("Operação invalida")
             print(f"Saldo {saldo}")
         else:
-            saldo = saldo - valores
-            numero_de_saques = numero_de_saques + 1
-            extrato.append(f'Saque: R${valores:.2f}')
-            print(f"Saldo {saldo}")
+            saldo, numero_de_saques = sacar(saldo, valores, numero_de_saques)
 
 
     elif opcao == 'e':
-        print("\n====== EXTRATO ======")
+        print("\n======= EXTRATO =======")
         if len(extrato) == 0:
             print("Nenhuma movimentação realizada.")
         else:
